113940a64ced5a8591a0c29f62a1c48c0cb6e452
maidy <maidyladino04@gmail.com>
2024-02-06 17:38:41 -0500

    se agregaron los atributos a la entidad ingreso y paciente

--- Back-end/src/main/java/consultorio/com/consultorio/models/Ingreso.java
+++ Back-end/src/main/java/consultorio/com/consultorio/models/Ingreso.java
@@ -1,5 +1,134 @@
 package consultorio.com.consultorio.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "Ingreso")
 public class Ingreso {
-    
+
+    /**
+     * id (UIID autogenerado)
+     * - habitacion(obligatorio)
+     * - cama(obligatorio)
+     * - paciente(obligatorio)
+     * - medico(obligatorio)
+     * - fecha de ingreso(obligatorio)
+     * - fecha de salida(obligatorio)
+     * - estado(obligatorio)
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "idIngreso", nullable = false, length = 36)
+    private String idIngreso;
+
+    @Column(name = "habitacion", nullable = false, length = 36)
+    private String habitacion;
+
+    @Column(name = "cama", nullable = false, length = 36)
+    private String cama;
+
+    @ManyToOne
+    @JoinColumn(name="idPaciente")
+    private Paciente Paciente;
+
+    @ManyToOne
+    @JoinColumn(name="idMedico")
+    private Medico Medico;
+
+    @Column(name = "fechaIngreso", nullable = false, length = 36)
+    private String fechaIngreso;
+
+    @Column(name = "fechaSalida", nullable = false, length = 36)
+    private String Celular;
+
+    @Column(name = "Estado", nullable = false, length = 36)
+    private String Estado;
+
+    public Ingreso() {
+    }
+
+    public Ingreso(String idIngreso, String habitacion, String cama,
+            consultorio.com.consultorio.models.Paciente paciente, consultorio.com.consultorio.models.Medico medico,
+            String fechaIngreso, String celular, String estado) {
+        this.idIngreso = idIngreso;
+        this.habitacion = habitacion;
+        this.cama = cama;
+        Paciente = paciente;
+        Medico = medico;
+        this.fechaIngreso = fechaIngreso;
+        Celular = celular;
+        Estado = estado;
+    }
+
+    public String getIdIngreso() {
+        return idIngreso;
+    }
+
+    public void setIdIngreso(String idIngreso) {
+        this.idIngreso = idIngreso;
+    }
+
+    public String getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(String habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public String getCama() {
+        return cama;
+    }
+
+    public void setCama(String cama) {
+        this.cama = cama;
+    }
+
+    public Paciente getPaciente() {
+        return Paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        Paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return Medico;
+    }
+
+    public void setMedico(Medico medico) {
+        Medico = medico;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String celular) {
+        Celular = celular;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
 }
+
--- Back-end/src/main/java/consultorio/com/consultorio/models/Paciente.java
+++ Back-end/src/main/java/consultorio/com/consultorio/models/Paciente.java
@@ -1,5 +1,159 @@
 package consultorio.com.consultorio.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "Paciente")
 public class Paciente {
+
+    /**
+     * id (UIID autogenerado)
+     * - documento de identidad (obligatorio)
+     * - primer nombre (obligatorio)
+     * - segundo nombre (opcional)
+     * - primer apellido (obligatorio)
+     * - segundo apellido (opcional)
+     * - celular (obligatorio)
+     * - correo electrónico (obligatorio)
+     * - nombre persona de contacto (obligatorio)
+     * - telefono persona de contacto (obligatorio)
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "idPaciente", nullable = false, length = 36)
+    private String idPaciente;
+
+    @Column(name = "documentoIdentidad", nullable = false, length = 36)
+    private String documentoIdentidad;
+
+    @Column(name = "primerNombre", nullable = false, length = 36)
+    private String primerNombre;
+
+    @Column(name = "segundoNombre", nullable = false, length = 36)
+    private String segundoNombre;
+
+    @Column(name = "primerApellido", nullable = false, length = 36)
+    private String primerApellido;
+
+    @Column(name = "segundoApellido", nullable = false, length = 36)
+    private String segundoApellido;
+
+    @Column(name = "Celular", nullable = false, length = 36)
+    private String Celular;
+
+    @Column(name = "Correo", nullable = false, length = 36)
+    private String Correo;
+
+    @Column(name = "nombrePersonaContacto", nullable = false, length = 36)
+    private String nombrePersonaContacto;
+
+    @Column(name = "telefonoPersonaContacto", nullable = false, length = 36)
+    private String telefonoPersonaContacto;
+
+    public Paciente() {
+    }
+
+    public Paciente(String idPaciente, String documentoIdentidad, String primerNombre, String segundoNombre,
+            String primerApellido, String segundoApellido, String celular, String correo, String nombrePersonaContacto,
+            String telefonoPersonaContacto) {
+        this.idPaciente = idPaciente;
+        this.documentoIdentidad = documentoIdentidad;
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        Celular = celular;
+        Correo = correo;
+        this.nombrePersonaContacto = nombrePersonaContacto;
+        this.telefonoPersonaContacto = telefonoPersonaContacto;
+    }
+
+    public String getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(String idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String celular) {
+        Celular = celular;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getNombrePersonaContacto() {
+        return nombrePersonaContacto;
+    }
+
+    public void setNombrePersonaContacto(String nombrePersonaContacto) {
+        this.nombrePersonaContacto = nombrePersonaContacto;
+    }
+
+    public String getTelefonoPersonaContacto() {
+        return telefonoPersonaContacto;
+    }
+
+    public void setTelefonoPersonaContacto(String telefonoPersonaContacto) {
+        this.telefonoPersonaContacto = telefonoPersonaContacto;
+    }
+
     
+    
+
 }
+
