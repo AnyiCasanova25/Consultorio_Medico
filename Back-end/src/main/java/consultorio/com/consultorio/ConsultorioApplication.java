13a4f2f8676a453109e8811acb205b7b7555a0d5
maidy <maidyladino04@gmail.com>
2024-02-06 16:17:36 -0500

    Se agrego informacion en controller, interfaces,interfaceService,models,service

--- Back-end/src/main/java/consultorio/com/consultorio/ConsultorioApplication.java
+++ Back-end/src/main/java/consultorio/com/consultorio/ConsultorioApplication.java
@@ -1,5 +1,5 @@
 package consultorio.com.consultorio;
-
+                                                                         
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsultorioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsultorioApplication.class, args);
+		SpringApplication.run(ConsultorioApplication.class, args);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 	}
+
+
+	
 
 }
