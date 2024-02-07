23f48b889e19a83c783281db640eec12a86ca13d
AnyiCasanova <anyi22195@gmail.com>
2024-02-07 14:52:05 -0500

    guardado

--- Back-end/src/main/java/consultorio/com/consultorio/Controller/medicoController.java
+++ Back-end/src/main/java/consultorio/com/consultorio/Controller/medicoController.java
@@ -14,10 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-=======
-
->>>>>>> 97c31b929456910ab91ab94506917e29b2acd1b9
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RequestMapping("/api/v1/Medico")
