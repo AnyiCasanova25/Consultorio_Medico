28b0021d65eabdb901de772434d845a76e1e154b
AnyiCasanova <anyi22195@gmail.com>
2024-02-07 13:02:40 -0500

    Llenar medicoController,Imedico,ImedicoService y medicoService

--- Back-end/src/main/java/consultorio/com/consultorio/interfaceService/ImedicoService.java
+++ Back-end/src/main/java/consultorio/com/consultorio/interfaceService/ImedicoService.java
@@ -1,5 +1,17 @@
 package consultorio.com.consultorio.interfaceService;
 
+import java.util.List;
+import java.util.Optional;
+
+import consultorio.com.consultorio.models.Medico;
+
 public interface ImedicoService {
-    
+
+    public String save(Medico Medico);
+
+    public List<Medico> findAll();
+
+    public Optional<Medico> findOne(String id);
+
+    public int delete(String id);
 }
--- Back-end/src/main/java/consultorio/com/consultorio/service/medicoService.java
+++ Back-end/src/main/java/consultorio/com/consultorio/service/medicoService.java
@@ -1,7 +1,42 @@
 package consultorio.com.consultorio.service;
 
-import consultorio.com.consultorio.interfaceService.ImedicoService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class medicoService implements ImedicoService{
-    
+import consultorio.com.consultorio.interfaceService.ImedicoService;
+import consultorio.com.consultorio.interfaces.Imedico;
+import consultorio.com.consultorio.models.Medico;
+
+@Service
+
+public class medicoService implements ImedicoService {
+
+    @Autowired
+    private Imedico data;
+
+    @Override
+    public String save(Medico Medico) {
+        data.save(Medico);
+        return Medico.getIdMedico();
+    }
+
+    @Override
+    public List<Medico> findAll() {
+        List<Medico> listaMedico = (List<Medico>) data.findAll();
+        return listaMedico;
+    }
+
+    @Override
+    public Optional<Medico> findOne(String id) {
+        Optional<Medico> Medico = data.findById(id);
+        return Medico;
+    }
+
+    @Override
+    public int delete(String id) {
+        data.deleteById(id);
+        return 1;
+    }
 }
