07694496cfb3544670da9471f4e1cf4b17067061
LUPA <LUPA@NEIDFPSITFSP477.FormacionSena.RED>
2024-02-20 12:44:44 -0500

    Version 2.0 RegistroMedico

--- fron-end/js/medico.js
+++ fron-end/js/medico.js
@@ -1,3 +1,120 @@
+/*
+var medicos = [];
+ Función para agregar un médico a la tabla y al array
+function AgregarMedico() {
+    var TipoDocumento = document.getElementById("TipoDocumento").value;
+    var NumeroDocumento = document.getElementById("NumeroDocumento").value;
+    var PrimerNombre = document.getElementById("PrimerNombre").value;
+    var SegundoNombre = document.getElementById("SegundoNombre").value;
+    var PrimerApellido = document.getElementById("PrimerApellido").value;
+    var SegundoApellido = document.getElementById("SegundoApellido").value;
+    var Telefono = document.getElementById("Telefono").value;
+    var Correo = document.getElementById("Correo").value;
+    var Direccion = document.getElementById("Direccion").value;
+    var Especialidad = document.getElementById("Especialidad").value;
+     Agregar el médico al array
+    medicos.push({ TipoDocumento: TipoDocumento, NumeroDocumento: NumeroDocumento, PrimerNombre: PrimerNombre, SegundoNombre: SegundoNombre, PrimerApellido: PrimerApellido, SegundoApellido: SegundoApellido, Telefono: Telefono, Correo: Correo, Direccion: Direccion, Especialidad: Especialidad });
+
+     Actualizar la tabla
+    actualizarTabla();
+}
+
+ Función para filtrar los médicos en la tabla
+function filtrarMedicos() {
+    var filtroEspecialidad = document.getElementById("filtro_especialidad").value.toLowerCase();
+    var tabla = document.getElementById("tabla_medicos");
+    var rows = tabla.getElementsByTagName("tr");
+
+    for (var i = 1; i < rows.length; i++) {
+        var Especialidad = rows[i].getElementsByTagName("td")[9].innerText.toLowerCase();
+        if (Especialidad.includes(filtroEspecialidad) || filtroEspecialidad === "") {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+
+ Función para actualizar la tabla de médicos
+function actualizarTabla() {
+    var tabla = document.getElementById("tabla_medicos");
+    tabla.innerHTML = `
+    <tr>
+        <th>TipoDocumento</th>
+        <th>NumeroDocumento</th>
+        <th>PrimerNombre</th>
+        <th>SegundoNombre</th>
+        <th>PrimerApellido</th>
+        <th>SegundoApellido</th>
+        <th>Telefono</th>
+        <th>Correo</th>
+        <th>Direccion</th>
+        <th>Especialidad</th>
+        <th>Acciones</th>
+        </tr>
+        `;
+
+    for (var i = 0; i < medicos.length; i++) {
+        tabla.innerHTML += `
+            <tr>
+                <td>${medicos[i].TipoDocumento}</td>
+                <td>${medicos[i].NumeroDocumento}</td>
+                <td>${medicos[i].PrimerNombre}</td>
+                <td>${medicos[i].SegundoNombre}</td>
+                <td>${medicos[i].PrimerApellido}</td>
+                <td>${medicos[i].SegundoApellido}</td>
+                <td>${medicos[i].Telefono}</td>
+                <td>${medicos[i].Correo}</td>
+                <td>${medicos[i].Direccion}</td>
+                <td>${medicos[i].Especialidad}</td>
+                <td>
+                    <button onclick="editarMedico(${i})">Editar</button>
+                    <button onclick="deshabilitarMedico(${i})">Deshabilitar</button>
+                    </td>
+                    </tr>
+                    `;
+    }
+}
+
+ Función para editar un médico
+function editarMedico(index) {
+    var nuevoTipoDocu = prompt("INGRESE EL NUEVO TIPO DE DOCUMENTO:", medicos[index].TipoDocumento);
+    var nuevoNumeroDocu = prompt("INGRESE EL NUEVO EL NUEVO NUMERO DE DOCUMENTO:", medicos[index].NumeroDocumento);
+    var nuevoPrimerNom = prompt("INGRESE EL NUEVO PRIMER NOMBRE:", medicos[index].PrimerNombre);
+    var nuevoSegundoNom = prompt("INGRESE EL NUEVO SEGUNDO NOMBRE:", medicos[index].SegundoNombre);
+    var nuevoPrimerApe = prompt("INGRESE EL NUEVO PRIMER APELLIDO:", medicos[index].PrimerApellido);
+    var nuevoSegundoApe = prompt("INGRESE EL NUEVO SEGUNDO APELLIDO:", medicos[index].SegundoApellido);
+    var nuevoTelefono = prompt("INGRESE EL NUEVO TELEFONO:", medicos[index].Telefono);
+    var nuevoCorreo = prompt("INGRESE EL NUEVO CORREO:", medicos[index].Correo);
+    var nuevoDireccion = prompt("INGRESE LA NUEVA DIRECCION:", medicos[index].Direccion);
+    var nuevoEspecialidad = prompt("INGRESE LA NUEVA ESPECIALIDAD:", medicos[index].Especialidad);
+     Actualizar los datos del médico en el array
+    if (nuevoTipoDocu !== null && nuevoNumeroDocu !== null && nuevoPrimerNom !== null && nuevoSegundoNom !== null && nuevoPrimerApe !== null && nuevoSegundoApe !== null && nuevoTelefono !== null && nuevoCorreo !== null && nuevoDireccion !== null && nuevoEspecialidad !== null) {
+        medicos[index].TipoDocumento = nuevoTipoDocu;
+        medicos[index].NumeroDocumento = nuevoNumeroDocu;
+        medicos[index].PrimerNombre = nuevoPrimerNom;
+        medicos[index].SegundoNombre = nuevoSegundoNom;
+        medicos[index].PrimerApellido = nuevoPrimerApe;
+        medicos[index].SegundoApellido = nuevoSegundoApe;
+        medicos[index].Telefono = nuevoTelefono;
+        medicos[index].Correo = nuevoCorreo;
+        medicos[index].Direccion = nuevoDireccion;
+        medicos[index].Especialidad = nuevoEspecialidad;
+
+         Actualizar la tabla
+        actualizarTabla();
+    }
+}
+
+ Función para deshabilitar un médico
+function deshabilitarMedico(index) {
+     Eliminar el médico del array
+    medicos.splice(index, 1);
+
+     Actualizar la tabla
+    actualizarTabla();
+}
+*/
 var medicos = [];
 // Función para agregar un médico a la tabla y al array
 function AgregarMedico() {
@@ -66,10 +183,10 @@ function actualizarTabla() {
                 <td>${medicos[i].Correo}</td>
                 <td>${medicos[i].Especialidad}</td>
                 <td>
-                <button onclick="editarMedico(${i})">Editar</button>
+                <button class="btn btn-primary" onclick="editarMedico(${i})">Editar</button>
                 </td>
                 <td>
-                <button onclick="deshabilitarMedico(${i})">Deshabilitar</button>
+                <button class="btn btn-primary" onclick="deshabilitarMedico(${i})">Deshabilitar</button>
                 </td>
                 </tr>
                     `;
