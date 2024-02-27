b3645c52f5c69413ba6ed6677f81f01c9d04f146
AnyiCasanova <anyi22195@gmail.com>
2024-02-27 15:01:17 -0500

    filtro en medico

--- Back-end/src/main/java/consultorio/com/consultorio/interfaces/Imedico.java
+++ Back-end/src/main/java/consultorio/com/consultorio/interfaces/Imedico.java
@@ -15,6 +15,6 @@ public interface Imedico extends CrudRepository< Medico , String>{
     
 
     //?1 es la primera variable (?2,?3 y asi sucesivamente)
-    @Query("SELECT m FROM Medico m WHERE m.primerNombre LIKE %?1%")
+    @Query("SELECT m FROM Medico m WHERE m.primerNombre LIKE %?1% OR m.documentoIdentidad LIKE %?2% OR m.Estado LIKE %?3%")
     List<Medico> filtroMedico(String filtro);
 }
