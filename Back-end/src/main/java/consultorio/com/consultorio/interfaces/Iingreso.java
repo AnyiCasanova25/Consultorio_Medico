dadf44d813235d03398b44b485d29bb9cbcd7df6
maidy <maidyladino04@gmail.com>
2024-02-28 14:41:28 -0500

    se cambio like por el igual

--- Back-end/src/main/java/consultorio/com/consultorio/interfaces/Iingreso.java
+++ Back-end/src/main/java/consultorio/com/consultorio/interfaces/Iingreso.java
@@ -12,7 +12,7 @@ import consultorio.com.consultorio.models.Ingreso;
 @Repository
 public interface Iingreso extends CrudRepository< Ingreso , String>{
 
-    @Query ("SELECT m FROM Ingreso m WHERE m.habitacion LIKE %?1% OR m.fechaIngreso LIKE %?1%")
+    @Query ("SELECT m FROM Ingreso m WHERE m.habitacion LIKE %?1% OR m.fechaIngreso = %?1%")
      List<Ingreso>filtroIngreso(String filtro); 
     
     @Query("SELECT m FROM Ingreso m WHERE m.Estado LIKE %?1% ")
