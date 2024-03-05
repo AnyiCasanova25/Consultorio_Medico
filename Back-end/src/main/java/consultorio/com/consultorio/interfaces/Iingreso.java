4ec423a3f8f5e01de062e6acd155afdf17161cae
maidy <maidyladino04@gmail.com>
2024-03-05 14:12:31 -0500

    se hizo parte del filtro ingreso

--- Back-end/src/main/java/consultorio/com/consultorio/interfaces/Iingreso.java
+++ Back-end/src/main/java/consultorio/com/consultorio/interfaces/Iingreso.java
@@ -8,13 +8,30 @@ import org.springframework.stereotype.Repository;
 
 import consultorio.com.consultorio.models.Ingreso;
 
-
 @Repository
-public interface Iingreso extends CrudRepository< Ingreso , String>{
+public interface Iingreso extends CrudRepository<Ingreso, String> {
 
-    @Query ("SELECT m FROM Ingreso m WHERE m.habitacion LIKE %?1% OR m.fechaIngreso = %?1%")
-     List<Ingreso>filtroIngreso(String filtro); 
-    
+    // @Query ("SELECT m FROM Ingreso m WHERE m.habitacion LIKE %?1% OR
+    // m.fechaIngreso = %?1%")
+    // List<Ingreso>filtroIngreso(String filtro);
+
     @Query("SELECT m FROM Ingreso m WHERE m.Estado LIKE %?1% ")
-    List<Ingreso>filtroIngreso(char estado);
+    List<Ingreso> filtroIngreso(char estado);
+
+    // @Query ("SELECT m,p,i from Ingreso i INNER JOIN Paciente p ON
+    // i.idPaciente=p.idPaciente\r\n" + //
+    // "INNER JOIN Medico m ON i.idMedico=m.idMedico\r\n" + //
+    // "WHERE\r\n" + //
+    // "p.primerNombre LIKE %?1% OR\r\n " + //
+    // "m.primerNombre LIKE %?1% OR\r\n " + //
+    // "i.habitacion = ?1 OR\r\n " + //
+    // "i.fechaIngreso = ?1")
+
+    @Query("SELECT i from Ingreso i JOIN i.Paciente p JOIN i.Medico m  WHERE\r\n" + //
+            "p.primerNombre LIKE %?1% OR\r\n " + //
+            "m.primerNombre LIKE %?1%  OR\r\n " + //
+            "i.habitacion = ?1 \r\n "  //
+            
+    )
+    List<Ingreso> filtroIngreso(String filtro);
 }
