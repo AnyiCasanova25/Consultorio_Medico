54d300b81aab0c62ddc7c1bdd9361c1f6152cd88
AnyiCasanova <anyi22195@gmail.com>
2024-03-12 13:12:00 -0500

    Se actualizaron los filtros en la entidad paciente  por nombres y apellidos

--- Back-end/src/main/java/consultorio/com/consultorio/interfaces/Ipaciente.java
+++ Back-end/src/main/java/consultorio/com/consultorio/interfaces/Ipaciente.java
@@ -13,8 +13,21 @@ import consultorio.com.consultorio.models.Paciente;
 public interface Ipaciente extends CrudRepository< Paciente , String>{
 
     // ?1 es la primera variable
+     /**
+     * id (UIID autogenerado)
+     * - documento de identidad (obligatorio)
+     * - primer nombre (obligatorio)
+     * - segundo nombre (opcional)
+     * - primer apellido (obligatorio)
+     * - segundo apellido (opcional)
+     * - celular (obligatorio)
+     * - correo electrónico (obligatorio)
+     * - nombre persona de contacto (obligatorio)
+     * - telefono persona de contacto (obligatorio)
+     * - Estado
+     */
 
-    @Query("SELECT m FROM Paciente m WHERE m.primerNombre LIKE %?1% OR documentoIdentidad LIKE %?1%")
+    @Query("SELECT m FROM Paciente m WHERE m.primerNombre LIKE %?1% OR m.segundoNombre LIKE %?1% OR m.primerApellido LIKE %?1% OR m.segundoApellido LIKE %?1% OR documentoIdentidad LIKE %?1%")
     List<Paciente>filtroPaciente(String filtro);
 
     @Query("SELECT m FROM Paciente m WHERE m.Estado LIKE %?1% ")
